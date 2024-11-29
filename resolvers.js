29a91fe67d9fca32fd3e14ed850a013c88311e23
Anusua Roy <anusua.roy05@gmail.com>
2024-11-29 22:26:04 +0530

    Initial commit

---
+++ resolvers.js
@@ -0,0 +1,83 @@
+import {
+  createJob,
+  deleteJob,
+  getJob,
+  getJobs,
+  getJobsByCompany,
+  updateJob,
+} from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
+
+export const resolvers = {
+  Query: {
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("No Job found with id " + id);
+      }
+      return job;
+    },
+    jobs: () => getJobs(),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No Company found with id " + id);
+      }
+      return company;
+    },
+  },
+  Mutation: {
+    createJob: (_root, { input: { title, description } }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("Missing Authentication!");
+      }
+      return createJob({ title, description, companyId: user.companyId });
+    },
+    deleteJob: async (_root, { id }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("Missing Authentication!");
+      }
+      const job = await deleteJob(id, user.companyId);
+      if (!job) {
+        throw notFoundError("No Job found with id " + id);
+      }
+      return job;
+    },
+    updateJob: async (_root, { input }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("Missing Authentication!");
+      }
+      const job = await updateJob(input, user.companyId);
+      if (!job) {
+        throw notFoundError("No Job found with id " + id);
+      }
+      return job;
+    },
+  },
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+  Job: {
+    company: (job) => {
+      return getCompany(job.companyId);
+    },
+    date: (job) => toISODate(job.createdAt),
+  },
+};
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
+}
+
+function unauthorizedError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "UNAUTHORIZED" },
+  });
+}
+
+function toISODate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
